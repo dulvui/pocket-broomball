bf4b2bc15a53b4b18f300067521729b11bb996be
Simon Dalvai <git@s9i.cc>
2023-05-28 14:42:50 +0200

    shop: add fade animation

--- game/src/ui/shop/Shop.gd
+++ game/src/ui/shop/Shop.gd
@@ -8,28 +8,33 @@ var current_index = {
 	"HELMET": 0,
 }
 
-onready var type_label = $Type/Label
+onready var type_label = $Content/Type/Label
+
+onready var animation_player:AnimationPlayer = $AnimationPlayer
 
 func _ready():
 	current_index["BALL"] = ShopUtil.items["BALL"]["selected"]
-	$CurrentItem.texture = ShopUtil.get_ball_texture()
-	$Ball.texture = ShopUtil.get_ball_texture()
-	$CenterContainer/VBoxContainer/Buy.text = "SELECTED"
+	$Content/CurrentItem.texture = ShopUtil.get_ball_texture()
+	$Content/Ball.texture = ShopUtil.get_ball_texture()
+	$Content/CenterContainer/VBoxContainer/Buy.text = "SELECTED"
+	animation_player.play("FadeIn")
 
 func _on_GoBack_pressed():
 	Global.click()
+	animation_player.play("FadeOut")
+	yield(animation_player, "animation_finished")
 	get_tree().change_scene("res://src/ui/menu/MenuScreen.tscn")
 
 func _on_Buy_pressed():
 	Global.click()
 	var success = ShopUtil.select(current_type, current_index[current_type])
 	
-	if $Locker.visible and success:
-		$Locker.play("open")
-		yield($Locker,"animation_finished")
-		$Locker.play("idle")
-		$Locker.hide()
-		$Price.hide()
+	if $Content/Locker.visible and success:
+		$Content/Locker.play("open")
+		yield($Content/Locker,"animation_finished")
+		$Content/Locker.play("idle")
+		$Content/Locker.hide()
+		$Content/Price.hide()
 
 	_update()
 
@@ -73,26 +78,26 @@ func _on_NextType_pressed():
 func _update():
 	# update button text
 	if current_index[current_type] == ShopUtil.items[current_type]["selected"]:
-		$CenterContainer/VBoxContainer/Buy.text = "SELECTED"
-		$Locker.hide()
-		$Price.hide()
+		$Content/CenterContainer/VBoxContainer/Buy.text = "SELECTED"
+		$Content/Locker.hide()
+		$Content/Price.hide()
 	elif current_index[current_type] in ShopUtil.items[current_type]["unlocked"]:
-		$CenterContainer/VBoxContainer/Buy.text = "SELECT"
-		$Locker.hide()
-		$Price.hide()
+		$Content/CenterContainer/VBoxContainer/Buy.text = "SELECT"
+		$Content/Locker.hide()
+		$Content/Price.hide()
 	else:
-		$Locker.show()
-		$Price.show()
-		$Price.text = str(ShopUtil.items[current_type]["list"][current_index[current_type]]["price"])
-		$CenterContainer/VBoxContainer/Buy.text = "BUY"
+		$Content/Locker.show()
+		$Content/Price.show()
+		$Content/Price.text = str(ShopUtil.items[current_type]["list"][current_index[current_type]]["price"])
+		$Content/CenterContainer/VBoxContainer/Buy.text = "BUY"
 	
 	# update textures
-	$AnimatedBody.change_style()
-	$CurrentItem.texture = ShopUtil.get_texture(current_type,current_index[current_type])
-	$Ball.texture = ShopUtil.get_ball_texture()
+	$Content/AnimatedBody.change_style()
+	$Content/CurrentItem.texture = ShopUtil.get_texture(current_type,current_index[current_type])
+	$Content/Ball.texture = ShopUtil.get_ball_texture()
 	
 	# scale if not ball
 	if current_type == "BALL":
-		$CurrentItem.scale = Vector2(4,4)
+		$Content/CurrentItem.scale = Vector2(4,4)
 	else:
-		$CurrentItem.scale = Vector2(8,8)
+		$Content/CurrentItem.scale = Vector2(8,8)
