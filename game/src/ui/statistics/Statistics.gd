055e0e9650af5ecf924dc4e2dbbbb2e2ff1fd02f
Simon Dalvai <info@simondalvai.org>
2023-10-31 23:34:06 +0100

    refactor hall of fame

--- game/src/ui/statistics/Statistics.gd
+++ game/src/ui/statistics/Statistics.gd
@@ -1,31 +1,18 @@
 extends Control
 
+onready var statistics:GridContainer = $Statistics
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	for league in Global.league_stats:
 		var name_label = Label.new()
 		name_label.set_text(league)
-		$GridContainer.add_child(name_label)
+		statistics.add_child(name_label)
 		
-		var played_label = Label.new()
-		played_label.set_text(str(Global.league_stats[league]["played"]))
-		$GridContainer.add_child(played_label)
+		var stats_label = Label.new()
+		var wins:int = Global.league_stats[league]["win"]
+		var played:int = Global.league_stats[league]["played"]
+		stats_label.set_text("%d/%d"%[wins,played])
+		statistics.add_child(stats_label)
 		
-		var win_label = Label.new()
-		win_label.set_text(str(Global.league_stats[league]["win"]))
-		$GridContainer.add_child(win_label)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 func _on_Menu_pressed():
 	get_tree().change_scene("res://src/ui/menu/MenuScreen.tscn")
