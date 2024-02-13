c8fbbfb531c26748f53dd115329dbff0a2d1b809
Simon Dalvai <info@simondalvai.org>
2024-02-13 19:31:27 +0100

    ball: limit max hit sounds to 20

--- game/src/actors/ball/HitSound.gd
+++ game/src/actors/ball/HitSound.gd
@@ -5,16 +5,20 @@
 #helper for multiple audio stream playback
 extends AudioStreamPlayer2D
 
+const max_hits:int = 20
+var hits:int = 0
 
 func hit(hit_speed:float) -> void:
 	pitch_scale = rand_range(0.8, 1.2)
 	volume_db = hit_speed / 1000.0
 	if !playing:
 		play()
-	else:
+	elif hits < max_hits:
+		hits += 1
 		var audio_stream_player:AudioStreamPlayer2D = self.duplicate(DUPLICATE_USE_INSTANCING)
 		get_parent().add_child(audio_stream_player)
 		audio_stream_player.stream = stream
 		audio_stream_player.play()
 		yield(audio_stream_player, "finished")
 		audio_stream_player.queue_free()
+		hits -= 1
