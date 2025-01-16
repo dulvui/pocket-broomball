1c9072d11f0397dabcd3f24bb87c08a1c5ca1a36
Simon Dalvai <info@simondalvai.org>
2025-01-16 09:14:26 +0100

    ball: make hit sound shorter and reduce concurrent sounds

--- game/src/actors/ball/HitSound.gd
+++ game/src/actors/ball/HitSound.gd
@@ -2,18 +2,17 @@
 
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
-#helper for multiple audio stream playback
 extends AudioStreamPlayer2D
 
-const max_hits:int = 20
-var hits:int = 0
+const MAX_HITS: int = 4
+var hits: int = 0
 
-func hit(hit_speed:float) -> void:
+func hit(hit_speed: float) -> void:
 	pitch_scale = rand_range(0.8, 1.2)
-	volume_db = hit_speed / 1000.0
+	volume_db = hit_speed / 1500.0
 	if !playing:
 		play()
-	elif hits < max_hits:
+	elif hits < MAX_HITS:
 		hits += 1
 		var audio_stream_player:AudioStreamPlayer2D = self.duplicate(DUPLICATE_USE_INSTANCING)
 		get_parent().add_child(audio_stream_player)
